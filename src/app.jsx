522593babeeadd795b39293f5ed85d96e6b1dabf
MouawiyyaShafiq <mouawiyya.sh@hotmail.com>
2025-01-20 14:09:03 +0100

    Paramétrage de sass et création composants header et footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import "./css/main.css"
 
 function App() {
   return (
     <BrowserRouter>
-
-        <Router/>
-    
+        <Header/>
+        <main>
+            <Router/>
+        </main>
+        <Footer/>
     </BrowserRouter>  
   );
 }
