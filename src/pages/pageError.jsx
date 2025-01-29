a07a3476dae52abc66e3db6d0843782ea37662aa
MouawiyyaShafiq <mouawiyya.sh@hotmail.com>
2025-01-29 08:55:40 +0100

    Integration page d'erreur

--- src/pages/pageError.jsx
+++ src/pages/pageError.jsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom"
+
 function PageError () {
 
-    return (<div>PageError</div>)
+    return (
+
+        <div className="pageErrorContainer">
+
+            <h1 className="pageErrorContainer_title">404</h1>
+            <p className="pageErrorContainer_text">Oups! La page que <br></br> vous demandez n'existe pas.</p>
+            <Link to="/" className="pageErrorContainer_link">Retourner sur la page d’accueil</Link>
+
+        </div>
+    
+    )
 
 }
 
